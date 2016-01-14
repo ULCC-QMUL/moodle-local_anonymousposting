ae899a036039b996d7d7b53444564275f09b3826
Doug Cooper <douglas.cooper@qmul.ac.uk>
2016-01-14 14:32:03 +0000

    Add plugin->component to version.php

--- version.php
+++ version.php
@@ -25,7 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2011122400;
+$plugin->version  = 2016011400;
 $plugin->requires = 2010112400;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 1.0;
+$plugin->component = 'local_anonymousposting';
